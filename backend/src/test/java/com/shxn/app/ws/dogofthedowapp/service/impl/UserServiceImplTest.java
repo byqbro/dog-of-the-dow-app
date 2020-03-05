96d5dde8e6ba5a19a9b8784288cb49047438a38d
Xianan Li <xli68@dons.usfca.edu>
2020-03-05 15:38:01 -0800

    test create user with exception

--- backend/src/test/java/com/shxn/app/ws/dogofthedowapp/service/impl/UserServiceImplTest.java
+++ backend/src/test/java/com/shxn/app/ws/dogofthedowapp/service/impl/UserServiceImplTest.java
@@ -1,19 +1,21 @@
 package com.shxn.app.ws.dogofthedowapp.service.impl;
 
+import com.shxn.app.ws.dogofthedowapp.exceptions.UserServiceException;
 import com.shxn.app.ws.dogofthedowapp.io.entity.UserEntity;
 import com.shxn.app.ws.dogofthedowapp.io.repositories.UserRepository;
+import com.shxn.app.ws.dogofthedowapp.shared.Utils;
 import com.shxn.app.ws.dogofthedowapp.shared.dto.UserDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 class UserServiceImplTest {
 
@@ -23,23 +25,37 @@ class UserServiceImplTest {
     @Mock
     UserRepository userRepository;
 
+    @Mock
+    Utils utils;
+
+    @Mock
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    String userId = "P8qmI1I2YfGL0ru4a0Yd0IuQXQx4XQ";
+    String encryptedPassword = "$2a$10$TiC5YPmWRmnGdih6NPHvS.Bmm8ziFSt9ZDz9EnsoCR5/xLgJIy68.";
+    String dateTimeNow = "2020-02-28 00:59:13 PST";
+
+    UserEntity userEntity;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
+
+        userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setUserId(userId);
+        userEntity.setUsername("peter");
+        userEntity.setEmail("peter@gmail.com");
+        userEntity.setEncryptedPassword(encryptedPassword);
+        userEntity.setFirstName("Peter");
+        userEntity.setLastName("Sun");
+        userEntity.setCreateAt(dateTimeNow);
+        userEntity.setUpdateAt("2020-02-29 00:59:13 PST");
     }
 
     @Test
     final void testGetUser() {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setUserId("P8qmI1I2YfGL0ru4a0Yd0IuQXQx4XQ");
-        userEntity.setUsername("peter");
-        userEntity.setEmail("peter@gmail.com");
-        userEntity.setEncryptPassword("$2a$10$TiC5YPmWRmnGdih6NPHvS.Bmm8ziFSt9ZDz9EnsoCR5/xLgJIy68.");
-        userEntity.setFirstName("Peter");
-        userEntity.setLastName("Sun");
-        userEntity.setCreateAt("2020-02-28 00:59:13 PST");
-        userEntity.setUpdateAt("2020-02-29 00:59:13 PST");
+
 
         when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
 
@@ -59,4 +75,46 @@ class UserServiceImplTest {
                 }
         );
     }
+
+    @Test
+    final void testCreateUserServiceException() {
+        when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
+        UserDto userDto = new UserDto();
+        userDto.setUsername("peter");
+        userDto.setEmail("peter@gmail.com");
+        userDto.setPassword("123456Ab");
+        userDto.setFirstName("Peter");
+        userDto.setLastName("Sun");
+
+        assertThrows(UserServiceException.class,
+                () -> {
+                    userService.createUser(userDto);
+                }
+        );
+    }
+
+    @Test
+    final void testCreateUser() {
+        when(userRepository.findByEmail(anyString())).thenReturn(null);
+        when(utils.generateUserId(anyInt())).thenReturn(userId);
+        when(utils.generateDateTimeNow()).thenReturn(dateTimeNow);
+        when(bCryptPasswordEncoder.encode(anyString())).thenReturn(encryptedPassword);
+        when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
+
+        UserDto userDto = new UserDto();
+        userDto.setUsername("peter");
+        userDto.setEmail("peter@gmail.com");
+        userDto.setPassword("123456Ab");
+        userDto.setFirstName("Peter");
+        userDto.setLastName("Sun");
+
+        UserDto storedUserDetails = userService.createUser(userDto);
+        assertNotNull(storedUserDetails);
+        assertEquals(userEntity.getFirstName(), storedUserDetails.getFirstName());
+        assertEquals(userEntity.getLastName(), storedUserDetails.getLastName());
+        assertNotNull(storedUserDetails.getUserId());
+        verify(bCryptPasswordEncoder, times(1)).encode("123456Ab");
+        verify(userRepository, times(1)).save(any(UserEntity.class));
+    }
+
 }
