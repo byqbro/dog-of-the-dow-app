2331eb6a640ca90ffefe5ad7fe15210ac9254c1a
Xianan Li <xiananli727@163.com>
2020-04-12 23:41:09 -0700

    update two different docker file for backend

--- backend/commands-dev.sh
+++ backend/commands-dev.sh
@@ -2,13 +2,10 @@
 
 #Execute Spring application
 CMD="java -jar target/app.jar"
-$CMD &
 SERVICE_PID=$!
 
 #Execute Tests
 mvn test
 
-#Wait for Spring execution
-wait "$SERVICE_PID"
-
-mvn spring-boot:run
+# #Wait for Spring execution
+# wait "$SERVICE_PID"
